77afa16754176fe1fa019ad05a9ceca05ed9ceb2
fallypse <arhepk@gmail.com>
2025-04-23 20:23:05 -0300

    Collisions

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -87,6 +87,11 @@ public class movement : MonoBehaviour
         */
 
         rb.AddForce(wishDir * accel);
+
+        if (rb.velocity.magnitude > topSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * topSpeed; // Limit the speed to the top speed
+        }
     }
 
     // Drag Calculations
